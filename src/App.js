e552de53e8dc2c055f5bd85087f038b9159e531d
najulxz <anajuliaalonso11@gmail.com>
2023-08-30 16:36:01 -0300

    finalizacao projeto

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   const [ listaTarefas, setListaTarefas ] = useState ([]);
   const [ tarefa, setTarefa ] = useState ({id:'', texto:"" , status:""});
 
+
+
   function addTarefa()
   {
     if( tarefa.texto !==""){
@@ -31,23 +33,46 @@ function App() {
   }, [listaTarefas])
 
   return (
-    <>
+    <section>
     <header>
-      <h1>react do</h1>
     </header>
-      <div>
-        <input type="text" name="tarefa" placeholder="digite sua tarefa" value={tarefa.texto} onChange={(e) => setTarefa({id: Math.random(), texto: e.target.value, status: false}) }/>
-        <button onClick={addTarefa}> +</button>
+      <div className="texto">
+           <input className="primeirotexto" type="text" placeholder="Insira um título" />   
       </div>
-      <div>
-        <ul>
-        {listaTarefas.map( (item, index) => (
-          <li  key={item.id}>{item.texto} <button onClick={() => statusTarefa(item.id, item.status )}>{item.status ? 'concluida': 'nao concluido'}</button>
-          <button onClick={() => excluirTarefa(item.id)}>X</button></li>
-        ))}
-        </ul>
+      <div className="caixaFlex">
+        <div className="lista">
+          <input type="text" name="tarefa" placeholder="digite sua tarefa" value={tarefa.texto} onChange={(e) => setTarefa({id: Math.random(), texto: e.target.value, status: false}) }/>
+          <i onClick={addTarefa}class="fa fa-arrow-down" aria-hidden="true"></i>
+
+        
+          <ul>
+          {listaTarefas.map( (item, index) => (
+            <li className={item.status ? "itemInativo" : "itemAtivo"} key={item.id}>{item.texto} <button onClick={() => statusTarefa(item.id, item.status )}>{item.status ? 
+            <i class="fa fa-check-square" aria-hidden="true"></i>
+            : <i class="fa fa-minus-square" aria-hidden="true"></i>
+          }</button>
+            <button onClick={() => excluirTarefa(item.id)}>  Remover</button></li>
+          ))}
+          </ul>
+        </div>
+        <div className="lista1">
+          <textarea  placeholder="Insira Observações"> </textarea>
+        </div>
       </div>
-    </>
+      
+      
+      
+      <div>
+        <textarea className="lista2" placeholder="Insira Observações Extras"> </textarea>
+      </div>
+
+     <p></p>
+
+        
+          
+
+      
+    </section>
   );
 }
 export default App;
